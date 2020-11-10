8e0448e919f0c6532fc80491efaa839d357e9004
Shannon <skotraba@gmail.com>
2020-11-10 17:43:36 -0500

    first commit

---
+++ src/components/navbar/MyNavbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar';
+import catLogo from "../../assets/blackCat.png";
+
+import "./MyNavbar.css";
+
+const MyNavbar = () => {
+    return (
+        <>
+        <Navbar  variant="dark" sticky="top" className="animate-navbar nav-theme justify-content-between">
+          <Navbar.Brand href="#home"></Navbar.Brand>
+            <img className="logo" src={catLogo} alt="dunno"></img>
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+          </Nav>
+        </Navbar>
+        <br />
+      </>
+    );
+}
+
+export default MyNavbar;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "styled-components";
+import {theme} from "./themes/theme";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    </ThemeProvider>,
   document.getElementById('root')
 );
 
