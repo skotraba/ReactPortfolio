eefd2641d78c626e1a7754dcdb6367544edc6d97
Shannon <skotraba@gmail.com>
2020-12-08 16:39:29 -0500

    made navbar responsive

--- src/components/navbar/MyNavbar.jsx
+++ src/components/navbar/MyNavbar.jsx
@@ -8,18 +8,22 @@ import "./MyNavbar.css";
 const MyNavbar = () => {
     return (
         <>
-        <Navbar variant="dark" fixed="top" className="animate-navbar nav-theme justify-content-between">
+        {
+        <Navbar  fixed="top" expand="lg" bg="dark" variant="dark" className="animate-navbar nav-theme justify-content-between">
           <Navbar.Brand href="#home"></Navbar.Brand>
             <a href="home"><img className="logo" src={Logo} alt="dunno"></img></a>
-          <Nav className="ml-auto">
-            <Nav.Link className="navLink" href="#home">Home</Nav.Link>
-            <Nav.Link className="navLink" href="#about">About</Nav.Link>
-            <Nav.Link className="navLink" href="#skills">Skills</Nav.Link>
-            <Nav.Link className="navLink" href="#experience">Experience</Nav.Link>
-            <Nav.Link className="navLink" href="#contact">Contact</Nav.Link>
-          </Nav>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="ml-auto">
+                <Nav.Link className="navLink" href="#home">Home</Nav.Link>
+                <Nav.Link className="navLink" href="#about">About</Nav.Link>
+                <Nav.Link className="navLink" href="#skills">Skills</Nav.Link>
+                <Nav.Link className="navLink" href="#experience">Experience</Nav.Link>
+                <Nav.Link className="navLink" href="#contact">Contact</Nav.Link>
+              </Nav>
+          </Navbar.Collapse>
         </Navbar>
-        <br />
+        }
       </>
     );
 }
