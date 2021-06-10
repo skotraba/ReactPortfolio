1fe309a6091b3cc6e486ee2aeee7bf95691ee6f6
Shannon <skotraba@gmail.com>
2021-06-10 13:58:34 -0400

    Updated projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,45 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import './Projects.scss';
 
 import {ProjectsData} from './ProjectsData.js';
-import TrelloGif from '../../assets/gifs/testTrello.gif';
+// import TrelloGif from '../../assets/gifs/testTrello.gif';
 
 const Projects = () => {
-  const featuredProject =
-   {
-    imageSrc: TrelloGif,
-    link: "https://github.com/skotraba/TrelloClone",
-    name: "Trello Clone",
-    description: "I created a clone of Trello using the React framework.  The project incorporates react-beautiful-dnd to recreate the drag and drop feature and material-ui for design.  The data is stored using Firebase/Firestore.  Written using BEM methodology and a mobile first design."
+
+  let projects = [...ProjectsData];
+
+  const [active, setActive] = useState(projects[0]);
+
+
+
+  const change = (index) =>{
+    let projectChange = active;
+    projectChange.active = false;
+    projectChange = projects[index];
+    projectChange.active = true;
+    setActive(projects[index]);
   }
 
   return (
     <div className="Projects" id="projects">
-      <div className="Projects__upper">
-        <h1 className="text-center Projects__heading">Projects</h1>
-        <div className="Projects__card-featured">
-          <img src={featuredProject.imageSrc} className="Projects__img-featured"/>
-          <div className="Projects__description-featured">
-            {/* {featuredProject.description} */}
-            <h3>{featuredProject.name}</h3>
-            <a className="Projects__btn" href={featuredProject.link} target="_blank">View Code</a>
+       <h1 className="text-center Projects__heading">Projects</h1>
+      <div className="Projects__container">
+        <div className="Projects__upper">
+          <div className="Projects__card">
+            <img src={active.imageSrc} className="Projects__img"/>
+            <div className="Projects__description">
+              <h3>{active.name}</h3>
+              <p> {active.description}</p>
+              <a className="Projects__btn" href={active.link} target="_blank">View Code</a>
+            </div>
           </div>
         </div>
+        <div class="pagination">
+
+          {
+            projects.map((project, index) => (
+              <a 
+              onClick={() => change(index)}
+              className={project.active ? "active" : ""}
+              >{index + 1}</a>
+            ))
+          }
+        </div>
       </div>
-      <div className="Projects__flex">
-        {ProjectsData.map(project => (
-           <div className="Projects__card">
-            <img src={project.imageSrc} className="Projects__img"/>
-              {/* {project.description} */}
-              <div className="Projects__overlay">
-                <h3>{project.name}</h3>
-                <a className="Projects__btn" href={project.link} target="_blank">View Code</a>
-              </div>
-            
-          </div>
-        ))}
-      </div>
+    
     </div>
       
   );
--- src/pages/projects/ProjectsData.js
+++ src/pages/projects/ProjectsData.js
@@ -15,38 +15,50 @@ import VerbViewer from '../../assets/images/projectImages/verbViewer.png';
 //TODO figure out why image quality is low
 
 //Gifs
-// import TrelloGif from '../../assets/gifs/testTrello.gif';
+import TrelloGif from '../../assets/gifs/testTrello.gif';
 
 export const ProjectsData = [
+  {
+    imageSrc: TrelloGif,
+    link: "https://github.com/skotraba/TrelloClone",
+    name: "Trello Clone",
+    description: "I created a clone of Trello using the React framework.  The project incorporates react-beautiful-dnd to recreate the drag and drop feature and material-ui for design.  The data is stored using Firebase/Firestore.  Written using BEM methodology and a mobile first design.",
+    active: true
+  },
   {
     imageSrc: VerbViewer,
     link: "https://skotraba.github.io/VerbViewer/",
     name: "Verb Viewer",
-    description: "I created a simple web application that has all the information about spanish verbs that I need in one place. I watch a lot of movies in Spanish to help me learn, and I often find myself wishing I had a central location for the information I look up.  I was really going for simplicity because some of the Spanish sites I use seem to overwhelm you with information you don't need. I used redux to store the current state, which in this application is the current verb being viewed. I found a free database of almost 600 spanish verbs that I used to create this application and implemented a search feature for the verbs shown on the left.  I would like to add a recently viewed section as well."
+    description: "I created a simple web application that has all the information about spanish verbs that I need in one place. I watch a lot of movies in Spanish to help me learn, and I often find myself wishing I had a central location for the information I look up.  I was really going for simplicity because some of the Spanish sites I use seem to overwhelm you with information you don't need. I used redux to store the current state, which in this application is the current verb being viewed. I found a free database of almost 600 spanish verbs that I used to create this application and implemented a search feature for the verbs shown on the left.  I would like to add a recently viewed section as well.",
+    active: false
   },
   {
     imageSrc: Burger,
     link: "https://github.com/skotraba/Burger-Builder",
     name: "Burger Builder",
-    description: "This was a project that I created from a Udemy Course called \"React - The Complete Guide.\" This project allows a user to order a customized burger that taught me a lot about the basics of using React.  I really enjoyed this course because it gave me a solid foundation of React fundamentals and I was then later able to create my own projects from what I learned in this course."
+    description: "This was a project that I created from a Udemy Course called \"React - The Complete Guide.\" This project allows a user to order a customized burger that taught me a lot about the basics of using React.  I really enjoyed this course because it gave me a solid foundation of React fundamentals and I was then later able to create my own projects from what I learned in this course.",
+    active: false
   },
   {
     imageSrc: GREVocab,
     link: "https://github.com/skotraba/GRE-Study",
     name: "GRE Vocab Study",
-    description: "This is a simple web application built using Electron to help study GRE vocabulary. The application cycles through words and allows the user to hide or show the definition.  Although, it is not a complex application, I really enjoyed creating this project because it was really interesting to work with Electron and use web languages to create an application outside of the browser.  Going back, I would probably spend more time on the design and make it look more professional.  I would love to be able to edit the lists as well. For example, add words you frequently miss so you can study those later."
+    description: "This is a simple web application built using Electron to help study GRE vocabulary. The application cycles through words and allows the user to hide or show the definition.  Although, it is not a complex application, I really enjoyed creating this project because it was really interesting to work with Electron and use web languages to create an application outside of the browser.  Going back, I would probably spend more time on the design and make it look more professional.  I would love to be able to edit the lists as well. For example, add words you frequently miss so you can study those later.",
+    active: false
   },
   {
     imageSrc: RecipeImage,
     link: "https://github.com/skotraba/RecipeCalculatorGUI",
     name: "Recipe Calculator",
-    description: "A program that allows the user to input ingredient info. The user can add ingredients listing calories and weight.  After adding recipes the user can view the recipe or calculate total calories and calories based on serving size.  It is a buffed up calcualtor program, but I love this because I do find it useful and use it when I need to calculate calories of homemade meals. Orignally written in Python, rewritten with GUI implementation in C# "
+    description: "A program that allows the user to input ingredient info. The user can add ingredients listing calories and weight.  After adding recipes the user can view the recipe or calculate total calories and calories based on serving size.  It is a buffed up calcualtor program, but I love this because I do find it useful and use it when I need to calculate calories of homemade meals. Orignally written in Python, rewritten with GUI implementation in C# ",
+    active: false
   },
   {
     imageSrc: Portfolio,
     link: "https://github.com/skotraba/skotraba.github.io",
     name: "Old Portfolio",
-    description: "Old portfolio I made focusing on design, responsive layout and utilizing Bootstrap. This was the second portfolio I have made out of many.  It was my first time trying to make my portfolio responsive, and I learned a lot of what not to do from this project.  I love being able to look back and see how far I've come in my software development journey."
+    description: "Old portfolio I made focusing on design, responsive layout and utilizing Bootstrap. This was the second portfolio I have made out of many.  It was my first time trying to make my portfolio responsive, and I learned a lot of what not to do from this project.  I love being able to look back and see how far I've come in my software development journey.",
+    active: false
   },
   // {
   //   imageSrc: ColorPicker,
